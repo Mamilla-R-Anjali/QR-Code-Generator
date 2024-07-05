ed7cce5e9a0bf3d5e6debbe4cf41689e90f9a8d2
Mamilla-R-Anjali <133332624+Mamilla-R-Anjali@users.noreply.github.com>
2024-07-05 22:21:24 +0530

    index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const qrbutton= document.getElementById('left');
 const qrimage= document.getElementById('qrimage');
  function  handleSubmit(event){
     event.preventDefault();
-    const input= qrtext.value;
+    const input= qrtext.value.trim();
 
 if(!input){
     alert('Please enter valid input');
 }
 else{
-qrimage.src= "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+ input;
+qrimage.src= "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+  encodeURIComponent(input);
 }
 }
 qrbutton.addEventListener('click', handleSubmit);
